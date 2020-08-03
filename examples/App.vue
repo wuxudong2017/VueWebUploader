043e5ecac9de014891af906e9fe18824843427da
吴旭东 <swuxudong@163.com>
2020-08-03 15:48:17 +0800

    jj

--- examples/App.vue
+++ examples/App.vue
@@ -1,15 +1,9 @@
 <template>
   <div id="app">
-    <VueWebUpload 
-    checkUrl="/storage/fdfs/checkFile" 
-    uploadUrl="/storage/fdfs/batchPeaceUpload"
-     tip="请选择上传的文件,大小500M以内"
-     :headers="{ 'Authorization': 'Bearer 2741145f-0065-4e37-9e24-a6d42ca22254'}"
-      :auto-upload="true" 
-      :size-limit="500*1024*1024" 
-      :chunk-size="1*1024*1024" 
-      :on-success="uploadSuccess"
-      :file-list="fileList"></VueWebUpload>
+    <VueWebUpload checkUrl="/storage/fdfs/checkFile" uploadUrl="/storage/fdfs/batchPeaceUpload" tip="请选择上传的文件,大小500M以内"
+      :headers="{ 'Authorization': 'Bearer 2741145f-0065-4e37-9e24-a6d42ca22254'}" :auto-upload="true"
+      :size-limit="500*1024*1024" :chunk-size="1*1024*1024" :on-success="uploadSuccess" :file-list="fileList">
+    </VueWebUpload>
   </div>
 </template>
 <script>
